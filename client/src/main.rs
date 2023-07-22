9a3c0a8464f87963e1cbff04784981682a3cc945
Light-JH <phoebehu@outlook.com>
2023-07-21 17:32:43 -0700

    set up simple client TCP

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,19 @@
+use std::io::Write;
+use std::net::SocketAddr;
+use std::str;
+use std::time::Duration;
+
+use mio::net::TcpStream;
+
+const SERVER_ADDR: &str = "127.0.0.1:8080";
+
 fn main() {
-    println!("Hello, world!");
+    //connect to the server
+    let server_addr: SocketAddr = SERVER_ADDR.parse().expect("Invalid server address");
+    let mut stream = TcpStream::connect(server_addr).expect("Failed to connect to server");
+
+    loop {
+        std::thread::sleep(Duration::from_millis(500));
+        stream.write_all(b"ping").expect("Failed to write");
+    }
 }
